719884372277169a6249567e8fb354841b3c6416
Rubens de Andrade <rubensaz14@gmail.com>
2022-03-05 00:22:29 -0300

    feat(core): added shared layout component

---
+++ challenges/web-frontend/src/app/shared/components/layout/layout.component.spec.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { LayoutComponent } from './layout.component';
+
+describe('LayoutComponent', () => {
+  let component: LayoutComponent;
+  let fixture: ComponentFixture<LayoutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LayoutComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a logo', () => {
+    const logo = fixture.debugElement.query(By.css('.c-layout__header-logo'));
+
+    expect(logo).toBeDefined();
+  });
+});
