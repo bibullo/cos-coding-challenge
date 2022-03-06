34f50e309be54af5e7ddc4d0f54fd53e18f4cced
Rubens de Andrade <rubensaz14@gmail.com>
2022-03-06 16:40:56 -0300

    feat(core): added lazy loaded routing to auctions and login pages

--- challenges/web-frontend/src/app/app-routing.module.ts
+++ challenges/web-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from './shared/components/layout/layout.component';
+
+import { LayoutComponent } from 'src/app/shared/components/layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    children: [
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./auth/auth.module').then((module) => module.AuthModule),
+      },
+      {
+        path: 'auctions',
+        loadChildren: () =>
+          import('./auctions/auctions.module').then(
+            (module) => module.AuctionsModule
+          ),
+      },
+    ],
   },
 ];
 
