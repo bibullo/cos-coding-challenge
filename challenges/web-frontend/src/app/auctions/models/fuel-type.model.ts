20dee2d4393ae77220b8d9a57f42b9c90a9cfed0
Rubens de Andrade <rubensaz14@gmail.com>
2022-03-08 20:12:26 -0300

    feat(auctions): added fuel and transmission types models

---
+++ challenges/web-frontend/src/app/auctions/models/fuel-type.model.ts
@@ -0,0 +1,11 @@
+export const fuelType: Record<number, string> = {
+  0: 'Unknown',
+  1: 'Gasoline',
+  2: 'Diesel  ',
+  4: 'Biodiesel',
+  3: 'Ethanol',
+  5: 'CNG',
+  6: 'LPG',
+  7: 'Hydrogen',
+  8: 'Electricity',
+};
---
+++ challenges/web-frontend/src/app/auctions/models/transmission-type.model.ts
@@ -0,0 +1,8 @@
+export const transmissionType: Record<number, string> = {
+  0: 'Unknown',
+  1: 'Manual transmission',
+  2: 'Automatic transmission',
+  4: 'Semi-automatic transmission',
+  3: 'CV transmission',
+  5: 'DC transmission',
+};
