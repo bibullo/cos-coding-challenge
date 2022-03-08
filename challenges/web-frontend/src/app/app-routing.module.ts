9206cd116bbce2f64a1d473f5c7fa4f47c301e5f
Rubens de Andrade <rubensaz14@gmail.com>
2022-03-07 21:54:18 -0300

    feat(app): added guards to app routing moddule

--- challenges/web-frontend/src/app/app-routing.module.ts
+++ challenges/web-frontend/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './auth/guards/auth/auth.guard';
+import { RolesGuard } from './auth/guards/roles/roles.guard';
+
 import { LayoutComponent } from 'src/app/shared/components/layout/layout.component';
 
 const routes: Routes = [
@@ -10,11 +13,17 @@ const routes: Routes = [
     children: [
       {
         path: 'login',
+        canActivate: [AuthGuard],
         loadChildren: () =>
           import('./auth/auth.module').then((module) => module.AuthModule),
       },
       {
         path: 'auctions',
+        canLoad: [AuthGuard],
+        canActivate: [RolesGuard],
+        data: {
+          allowedRoles: ['salesman'],
+        },
         loadChildren: () =>
           import('./auctions/auctions.module').then(
             (module) => module.AuctionsModule
